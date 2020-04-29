e326dc1aa19cf4e712b41aac08f9a465b24fa02d
Rudolf Fanchini <43999588+FanchiniRudolf@users.noreply.github.com>
2020-04-29 19:26:00 -0400

    ios app km to miles

---
+++ ConverterKmMiles/ConverterKmMiles/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConverterKmMiles
+//
+//  Created by user167967 on 4/29/20.
+//  Copyright © 2020 ITESM. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
