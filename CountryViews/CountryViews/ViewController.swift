693252ffa2b10ad8fbe8e3a135039106da212256
Rudolf Fanchini <43999588+FanchiniRudolf@users.noreply.github.com>
2020-05-13 17:23:33 -0400

    new ios activity

---
+++ CountryViews/CountryViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountryViews
+//
+//  Created by user167967 on 5/13/20.
+//  Copyright © 2020 ITESM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
