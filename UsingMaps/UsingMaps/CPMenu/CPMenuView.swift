ccc7f8bec97677a4e50517968e05312107867875
Rudolf Fanchini <43999588+FanchiniRudolf@users.noreply.github.com>
2020-06-03 16:50:05 -0400

    added menu

---
+++ UsingMaps/UsingMaps/CPMenu/CPMenuView.swift
@@ -0,0 +1,190 @@
+//
+//  CPMenuView.swift
+//  CPMenuView
+//
+//  Created by framgia on 2/22/17.
+//  Copyright © 2017 framgia. All rights reserved.
+//
+
+import UIKit
+
+public enum CPMenuType {
+    case all
+    case half
+    case upperhalf
+    case lowerhalf
+    case quarter
+}
+
+public enum CPMenuViewState {
+    case none
+    case expand
+}
+
+public protocol CPMenuViewDelegate: class {
+    func menuView(_ menuView: CPMenuView, didSelectButtonAtIndex index: Int)
+    func menuView(_ menuView: CPMenuView, didSelectHomeButtonState state: CPMenuViewState)
+}
+
+public protocol CPMenuViewDataSource: class {
+    func menuViewNumberOfItems() -> Int
+    func menuView(_ : CPMenuView, buttonAtIndex index: Int) -> SubMenuButton
+}
+
+public class CPMenuView {
+
+    fileprivate var parentView: UIView?
+    fileprivate var homeButton: HomeMenuButton?
+    fileprivate var animator: CPMenuAnimator?
+    fileprivate var menuButtons:[SubMenuButton] = []
+
+    public var type: CPMenuType = .all
+    public var isClockWise = true // Default is clockwise
+    public var radius: Double = 100 // Default radius
+
+    fileprivate(set) var state: CPMenuViewState = .none {
+        didSet {
+            state == .none ? animator?.animationHideSubMenuButton(subButtons: menuButtons, completion: nil) : animator?.animationShowSubMenuButton(subButtons: menuButtons, completion: nil)
+            animator?.animationHomeButton(homeButton: homeButton!, state: state, completion: nil)
+            setHomeButtonImage(pressed: state == .expand)
+        }
+    }
+
+    public var datasource: CPMenuViewDataSource?
+    public var delegate: CPMenuViewDelegate?
+
+
+
+    public init(parentView: UIView, homeButton: HomeMenuButton, animator: CPMenuAnimator ,type: CPMenuType,radius: Double = 100, isClockWise: Bool) {
+        self.parentView = parentView
+        self.homeButton = homeButton
+        self.animator = animator
+        self.type = type
+        self.radius = radius
+        self.isClockWise = isClockWise
+        setup()
+    }
+
+    convenience public init(parentView: UIView, homeButton: HomeMenuButton, type: CPMenuType = .all ,radius: Double = 100, isClockWise: Bool = true) {
+        self.init(parentView: parentView, homeButton: homeButton, animator: CPMenuAnimator(), type: type, radius: radius, isClockWise: isClockWise)
+    }
+
+    private func setup() {
+        setUpHomeButton()
+    }
+
+    fileprivate func setUpHomeButton() {
+        guard let parentView = parentView else {
+            return
+        }
+        setUpDefaultHomeButtonPosition()
+        homeButton?.delegate = self
+        parentView.addSubview(homeButton!)
+        parentView.bringSubviewToFront(homeButton!)
+    }
+
+    fileprivate func setUpDefaultHomeButtonPosition() {
+        guard let parentView = parentView else {
+            return
+        }
+        homeButton?.center = parentView.center
+    }
+
+
+    public func reloadButton() {
+        state = .none
+        removeButton()
+        addButton()
+        layoutButton()
+    }
+
+    private func addButton() {
+        guard let numberOfItem = datasource?.menuViewNumberOfItems() else { return }
+        for i in 0..<numberOfItem {
+            let button = datasource!.menuView(self, buttonAtIndex: i)
+            button.delegate = self
+            parentView?.addSubview(button)
+            parentView?.bringSubviewToFront(button)
+            menuButtons.append(button)
+
+        }
+    }
+
+    private func removeButton() {
+        let _ =  menuButtons.map { $0.removeFromSuperview() }
+        menuButtons.removeAll()
+    }
+
+    private func layoutButton() {
+        let theta = getTheta()
+        let flip: Double = isClockWise ? 1 : -1
+        var index = 0
+        let center = CGPoint(x: homeButton!.frame.midX, y: homeButton!.frame.midY)
+        menuButtons.forEach { (item) in
+            var x = 0.0
+            var y = 0.0
+            if type == .upperhalf  {
+                x = Double(center.x) - cos(Double(index) * theta) * radius * flip
+                y = Double(center.y) - sin(Double(index) * theta) * radius
+
+            } else if type == .lowerhalf {
+                x = Double(center.x) + cos(Double(index) * theta) * radius * flip
+                y = Double(center.y) + sin(Double(index) * theta) * radius
+
+            } else {
+                x = Double(center.x) + sin(Double(index) * theta) * radius * flip
+                y = Double(center.y) - cos(Double(index) * theta) * radius
+            }
+
+            item.center = center
+            item.startPosition = center
+            item.endPosition = CGPoint(x: x, y: y)
+            item.index = index
+            item.alpha = 0
+            index += 1
+        }
+
+    }
+
+    private func getTheta() -> Double {
+        let numberItem: Double = Double(datasource!.menuViewNumberOfItems() == 0 ? 1 : datasource!.menuViewNumberOfItems() - 1)
+        switch type {
+        case .all:
+            return 2 * .pi / (numberItem + 1)
+        case .half:
+            return .pi / numberItem
+        case .upperhalf:
+            return .pi / numberItem
+        case .lowerhalf:
+            return .pi / numberItem
+        case .quarter:
+            return .pi/2 / numberItem
+        }
+    }
+
+    private func setHomeButtonImage(pressed: Bool) {
+        homeButton?.markAsPressed(pressed)
+    }
+
+
+    public func setHomeButtonPosition(position: CGPoint) {
+        homeButton?.center = position
+        reloadButton()
+    }
+
+
+}
+
+extension CPMenuView: CPMenuButtonDelegate {
+    public func didSelectButton(sender: CPMenuButton) {
+        if let subMenuButton = sender as? SubMenuButton, let indexOfItem = menuButtons.firstIndex(of: subMenuButton) {
+            state = .none
+            delegate?.menuView(self, didSelectButtonAtIndex: indexOfItem)
+        } else {
+            state = state == .none ? .expand : .none
+            delegate?.menuView(self, didSelectHomeButtonState: state)
+        }
+    }
+}
+
+
---
+++ UsingMaps/UsingMaps/ViewController.swift
@@ -0,0 +1,148 @@
+//
+//  ViewController.swift
+//  UsingMaps
+//
+//  Created by user167967 on 5/27/20.
+//  Copyright © 2020 ITESM. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+
+class ViewController: UIViewController {
+    
+    let arrImages = ["linux", "people", "pac-man", "pokemon"]
+    
+    @IBOutlet weak var map: MKMapView!
+    let gps = CLLocationManager()
+    var showingRestaurants = false
+    var menu: CPMenuView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureMap()
+        configureMenu()
+    }
+    
+    func configureMenu(){
+        let btnMenu = HomeMenuButton(image:  UIImage(named: "menu")!, size: CGSize(width: 50, height: 50))
+        btnMenu.pressedImage = UIImage(named: "close")!
+        
+        let animator = CPMenuAnimator(commonDuration: 0.5, commonSpringWithDamping: 0.5, commonSpringVelocity: 10)
+        
+        menu = CPMenuView(parentView: self.view, homeButton: btnMenu, animator: animator, type: .all, isClockWise: true)
+        menu.delegate = self
+        menu.datasource = self
+        menu.setHomeButtonPosition(position: CGPoint(x: self.view.center.x, y: self.view.frame.height-180))
+    }
+    
+    @IBAction func changeTraffic(_ sender: Any) {
+        map.showsTraffic = !map.showsTraffic
+    }
+    @IBAction func showFoodPlaces(_ sender: Any) {
+        if showingRestaurants {
+            self.map.removeAnnotations(self.map.annotations)
+            showingRestaurants = !showingRestaurants
+        }else{
+            let request = MKLocalSearch.Request()
+            request.naturalLanguageQuery = "Restaurant"
+            request.region = map.region
+            
+            let search = MKLocalSearch(request: request)
+            
+            search.start(completionHandler: {(response, error)in
+                if error != nil{
+                    print("Error ocurred in search : \(error!.localizedDescription)")
+                }else if response!.mapItems.count == 0 {
+                    print("No matches found")
+                }else{
+                    print("Matches found")
+                    for item in response!.mapItems {
+                        let foodPlace = CLLocationCoordinate2DMake(item.placemark.coordinate.latitude, item.placemark.coordinate.longitude)
+                        let pin = MKPointAnnotation()
+                        pin.coordinate = foodPlace
+                        pin.title = item.name
+                        self.map.addAnnotation(pin)
+                    }
+                }
+            })
+            showingRestaurants = !showingRestaurants
+        }
+        
+        
+    }
+    func configureMap (){
+        map.delegate = self
+        gps.delegate = self
+        
+        gps.desiredAccuracy = kCLLocationAccuracyBest
+        gps.requestWhenInUseAuthorization()
+        
+        let centre = CLLocationCoordinate2DMake(19.5953, -99.2276)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: centre, span: span)
+        
+        map.region = region
+        
+        
+        
+    }
+
+}
+
+extension ViewController: CPMenuViewDelegate, CPMenuViewDataSource{
+    func menuView(_ menuView: CPMenuView, didSelectButtonAtIndex index: Int) {
+        print("Selecciono \(index)")
+    }
+    
+    func menuView(_ menuView: CPMenuView, didSelectHomeButtonState state: CPMenuViewState) {
+        
+    }
+    
+    func menuViewNumberOfItems() -> Int {
+        return arrImages.count
+    }
+    
+    func menuView(_: CPMenuView, buttonAtIndex index: Int) -> SubMenuButton {
+        let imgBtn = UIImage(named: arrImages[index])!
+        let btnSubMenu = SubMenuButton(image: imgBtn, size: CGSize(width: 60, height: 60))
+        btnSubMenu.offset = 20
+        btnSubMenu.backgroundColor = UIColor(displayP3Red: 0.5, green: 0.4, blue: 0.7, alpha: 0.5)
+        btnSubMenu.layer.cornerRadius = btnSubMenu.frame.height/2
+        btnSubMenu.layer.masksToBounds = true
+        
+        return btnSubMenu
+        
+    }
+    
+    
+}
+
+extension ViewController: CLLocationManagerDelegate, MKMapViewDelegate {
+    	
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        mapView.setCenter(userLocation.coordinate, animated: true)
+        
+        let borregos = CLLocationCoordinate2DMake(userLocation.coordinate.latitude, userLocation.coordinate.longitude)
+        let pin = MKPointAnnotation()
+        pin.coordinate = borregos
+        pin.title = "Plaza borregos"
+        pin.subtitle = "Tec CEM"
+        map.addAnnotation(pin)
+    }
+    
+     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse:
+            gps.startUpdatingLocation()
+        case .denied:
+            print("NO permit")
+        default:
+            print("indeterminate")
+        }
+    }
+}
