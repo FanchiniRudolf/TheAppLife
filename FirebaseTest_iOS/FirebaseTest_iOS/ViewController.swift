91899c81a0f28404acfb5c7c089768acee040e15
Rudolf Fanchini <43999588+FanchiniRudolf@users.noreply.github.com>
2020-06-04 19:23:54 -0400

    start firebase ios

---
+++ FirebaseTest_iOS/FirebaseTest_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseTest_iOS
+//
+//  Created by user167967 on 6/4/20.
+//  Copyright © 2020 ITESM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
